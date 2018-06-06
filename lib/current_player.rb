92cd3bac8d9b23c54f52f79d401d346e69b325b4
Josh Mulloy <joshmulloy1@gmail.com>
2018-06-06 18:58:02 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -11,13 +11,12 @@ end
   return counter
 end
 def current_player(board)
-  if turn_count(board) %  == 0
+  #if turn_count(board) %  == 0
     num = turn_count(board)
     if num % 2 == 0
       return "X"
     else
       return "O" 
-    puts "divisible by 2"
+    #puts "divisible by 2"
     end 
-  end
 end 

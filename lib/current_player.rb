b17ff2fd8719ed03f9d778820d26ace39dd9fc30
Josh Mulloy <joshmulloy1@gmail.com>
2018-06-06 18:37:35 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,31 +1,6 @@
 def turn_count(board)
   counter = 0 
   counter += 1
-  turn_count.each do |board|
+  board.each do |space|
   end
 end
-def turn_count(turn)
-  counter = 1
-  move_count = []
-  turn.each do |unit|
-    counter = 0
-    if unit == "X" || unit == "O"
-      puts "spot taken"
-      move_count.push(1)
-    else 
-      puts "spot free"
-      counter += 1
-    end 
-def current_player(board)
-  #if turn_count(board) %  == 0
-    num = turn_count(board)
-    if num % 2 == 0
-      return "X"
-    else
-      return "O" 
-    #puts "divisible by 2"
-    end 
-end 
-  end 
-  move_count.length.to_i
-end 
